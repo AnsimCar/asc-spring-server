77eed0f235fb9e37dea211897025e297b0b9b1d7
woohyeonjoe <qlqkthd8802@gmail.com>
2023-04-06 15:55:50 +0900

    #6 - 입력 데이터 검증 개발 및 이에 따라 발생하는 에러를 표시하는  뷰 개발
    *AccountDto는 register 메서드의 검증 통과 데이터는 유지하는 로직(@ModelAttribute("account"))을 추가하면서 뷰에서 모델에 접근하는 코드가 강제되었다. 
    그래서 빈 모델이라도 넘겨주어야 뷰 이동이 가능해져서 AccountDto를 만들었다.
    우선 기본틀만 만들어서 빈 모델을 넘겨주는 용도로 사용했지만 추후에 진짜 Dto로 사용하려면 mapper 기능을 하는 메서드를 추가하든 추가 작업이 필요할 것 같다.

--- src/main/java/project/asc/AnsimCar/controller/AccountController.java
+++ src/main/java/project/asc/AnsimCar/controller/AccountController.java
@@ -2,15 +2,16 @@ package project.asc.AnsimCar.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import project.asc.AnsimCar.domain.Account;
 import project.asc.AnsimCar.dto.account.AccountRequest;
+import project.asc.AnsimCar.dto.account.AccountDto;
 import project.asc.AnsimCar.service.AccountService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -45,17 +46,29 @@ public class AccountController {
 
     /**
      * 뷰 이동(register)
+     * ---
+     * register 메서드의 검증 통과 데이터는 유지하는 로직(@ModelAttribute("account"))을 추가하면서 뷰에서 모델에 접근하는 코드가 강제되었다.
+     * 그래서 빈 모델이라도 넘겨주어야 뷰 이동이 가능해졌다.
      */
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+
+        model.addAttribute("account", new AccountDto());
+
         return "account/register";
     }
 
     /**
      * register 컨트롤러
+     * ---
+     * 입력 데이터 검증 로직을 추가하면서 만약 입력데이터 중 몇개의 데이터만 검증을 통과하였다면 통과한 데이터는 유지해야 한다.
+     * 그래서 기존에는 사용하지 않던 @ModelAttribute("account")를 추가하고 뷰에서 검증 통과 데이터를 유지할 수 있게 추가 개발하였다.
      */
     @PostMapping("/register")
-    public String register(AccountRequest accountRequest) {
+    public String register(@Validated @ModelAttribute("account") AccountRequest accountRequest, BindingResult bindingResult) {
+
+        //입력 데이터 검증에 실패하면 다시 입력 폼으로
+        if(bindingResult.hasErrors()) return "account/register";
 
         accountService.register(accountRequest);
 
