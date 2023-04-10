f961e521292c9557cd7d0daaf459a0a62886149b
woohyeonjoe <qlqkthd8802@gmail.com>
2023-04-10 16:49:07 +0900

    #12 - 공통 컬럼으로 사용할 클래스 생성 및 설정

---
+++ src/main/java/project/asc/AnsimCar/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package project.asc.AnsimCar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+}
---
+++ src/main/java/project/asc/AnsimCar/domain/common/BaseEntity.java
@@ -0,0 +1,28 @@
+package project.asc.AnsimCar.domain.common;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    private LocalDateTime createdAt;                            //생성일시
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;                           //수정일시
+
+}
