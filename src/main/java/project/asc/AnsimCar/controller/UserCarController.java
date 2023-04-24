e0f8ca237acc33acd4bed8f761b3cfd327bbcb5b
oddnine <dnwnsgur12@gmail.com>
2023-04-24 16:10:49 +0900

    #34 - 차량 CRD 컨트롤러 및 뷰

---
+++ src/main/java/project/asc/AnsimCar/controller/UserCarController.java
@@ -0,0 +1,133 @@
+package project.asc.AnsimCar.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import project.asc.AnsimCar.authentication.AccountContext;
+import project.asc.AnsimCar.domain.Account;
+import project.asc.AnsimCar.domain.type.CarCategory;
+import project.asc.AnsimCar.domain.type.Fuel;
+import project.asc.AnsimCar.dto.account.response.AccountResponse;
+import project.asc.AnsimCar.dto.usercar.request.UserCarCreateRequest;
+import project.asc.AnsimCar.dto.usercar.request.UserCarUpdateRequest;
+import project.asc.AnsimCar.dto.usercar.response.UserCarResponse;
+import project.asc.AnsimCar.service.UserCarService;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/usercar")
+@RequiredArgsConstructor
+public class UserCarController {
+
+    private final UserCarService userCarService;
+
+    /**
+     * enum
+     */
+    @ModelAttribute("carCategories")
+    public CarCategory[] carCategories() {
+        return CarCategory.values();
+    }
+
+    @ModelAttribute("fuels")
+    public Fuel[] fuels() {
+        return Fuel.values();
+    }
+
+    /**
+     * GET 차량 등록
+     */
+    @GetMapping("/add")
+    public String addCar(Model model) {
+
+        model.addAttribute("userCar", UserCarCreateRequest.builder().build());
+
+        return "usercar/addUserCar";
+    }
+
+    /**
+     * POST 차량 등록
+     */
+    @PostMapping("/add")
+    public String addCar(@Validated @ModelAttribute("userCar") UserCarCreateRequest userCarCreateRequest,
+                         Authentication authentication,
+                         BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) return "usercar/addUserCar";
+
+        AccountContext accountContext = (AccountContext) authentication.getPrincipal();
+        Account account = accountContext.getAccount();
+
+        userCarService.addUserCar(account.getId(), userCarCreateRequest);
+
+        return "redirect:/usercar/list";
+    }
+
+    /**
+     * 차량 목록
+     */
+    @GetMapping("/list")
+    public String carList(Authentication authentication, Model model) {
+        AccountContext accountContext = (AccountContext) authentication.getPrincipal();
+        Account account = accountContext.getAccount();
+
+        model.addAttribute("userCarList", userCarService.findByAccountId(account.getId()));
+
+        return "usercar/list";
+    }
+
+    /**
+     * 차량 상세 정보
+     */
+    @GetMapping("/list/")
+    public String carDetail(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("userCar", userCarService.findById(id));
+
+        return "usercar/info";
+    }
+
+    /**
+     * 차량 수정
+     */
+    @GetMapping("/update")
+    public String updateCar(@RequestParam("id") Long id, Model model) {
+
+        model.addAttribute("updateCar", userCarService.findById(id));
+
+        return "usercar/update";
+    }
+
+    @PostMapping("/update")
+    public String updateCar(@RequestParam("id") Long id,
+                            @Validated @ModelAttribute("updateCar") UserCarUpdateRequest userCarUpdateRequest,
+                            Authentication authentication,
+                            BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) return "usercar/info";
+
+        AccountContext accountContext = (AccountContext) authentication.getPrincipal();
+        Account account = accountContext.getAccount();
+
+        userCarService.updateUserCar(account.getId(), id, userCarUpdateRequest);
+
+        return "usercar/update";
+    }
+
+    /**
+     * 차량 삭제
+     */
+    @GetMapping("/delete")
+    public String deleteCar(@RequestParam("id") Long id, Authentication authentication) {
+        AccountContext accountContext = (AccountContext) authentication.getPrincipal();
+        Account account = accountContext.getAccount();
+
+        userCarService.deleteUserCar(account.getId(), id);
+
+        return "redirect:/usercar/list";
+    }
+}
--- src/main/java/project/asc/AnsimCar/dto/usercar/request/UserCarUpdateRequest.java
+++ src/main/java/project/asc/AnsimCar/dto/usercar/request/UserCarUpdateRequest.java
@@ -1,5 +1,6 @@
 package project.asc.AnsimCar.dto.usercar.request;
 
+import lombok.Builder;
 import lombok.Data;
 import project.asc.AnsimCar.domain.type.CarCategory;
 import project.asc.AnsimCar.domain.type.Fuel;
@@ -11,18 +12,19 @@ public class UserCarUpdateRequest {
     @NotBlank(message = "모델명을 입력해주세요.")
     String carModel;
 
-    @NotBlank(message = "차종을 입력해주세요.")
+//    @NotBlank(message = "차종을 입력해주세요.")
     CarCategory carCategory;
 
     @NotBlank(message = "제조사를 입력해주세요.")
     String manufacturer;
 
-    @NotBlank(message = "연료를 입력해주세요.")
+//    @NotBlank(message = "연료를 입력해주세요.")
     Fuel fuel;
 
     private UserCarUpdateRequest() {
     }
 
+    @Builder
     public UserCarUpdateRequest(final String carModel, final CarCategory carCategory, final String manufacturer, final Fuel fuel) {
         this.carModel = carModel;
         this.carCategory = carCategory;
